1aceb50cf17fc2cfc2232926a3988a4252664c83
KinsleyDean <122942291+KinsleyDean@users.noreply.github.com>
2025-01-27 13:16:21 -0700

    homework2

---
+++ HW2/script.js
@@ -0,0 +1,20 @@
+//First try of making JS work...goodluck to me
+console.log('My Three Favorite Websites:')
+console.log('Youtube.com')
+console.log('Disney Plus.com')
+console.log('FreeSound.com')
+console.log('             ') //this is meant hopefully to give a space between each section
+console.log('My Three Favorite Games:')
+console.log("Clue")
+console.log("Cards Against Humanity")
+console.log('"SORRY!"')
+console.log("                ")//this is meant hopefully to give a space between each section
+console.log("My Favorite Artists")
+console.log("Hayao Miyazaki:Introduced Japanese animation to the world, inspired nimators, paved the way for other non-Western animated films")
+console.log(" Katy Perry:One of the best selling pop artist in history, biggest influence in her 'Camp Style' in pop music,  ")
+console.log("Sam Smith:Fashion innovator, LGBTQIA+ advocate, Self-expression advocate")
+
+// I hope this is correct, idk though cause I needed help from CGPT, go over with Cassens
+const question = "What is your favorite color?";
+const answer = prompt(question); 
+console.log(`Your favorite color is: ${answer}`);
