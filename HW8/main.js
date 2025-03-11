f10cd4f26b94fcf4dfcfe79df85a75a86f535e96
KinsleyDean <122942291+KinsleyDean@users.noreply.github.com>
2025-03-10 20:06:26 -0600

    try1

---
+++ HW8/main.js
@@ -0,0 +1,192 @@
+
+
+class ViewFinder
+{
+    constructor(Image, Description, Shape)
+    {
+		this.Image = Image;
+		this.Description = Description;
+		this.Shape = Shape;
+        
+    }
+    showInformation()
+    {
+        document.getElementById("Viewfinder").innerHTML =  "<img src= " + this.Image + "> <br>Description:" + this.Description;
+    }
+
+    
+    // keep in mind only one return statement can exist in a function
+    toString()
+    {
+    let str; console.log(this.Image)
+    str = `<img src=  ${this.Image} ><br>Description: ${this.Description}`;
+    return str;
+    }
+
+
+
+/*
+//
+    get theImage()
+    {
+        return this.Image;
+    }
+    set theImage(Image) 
+    {
+        this.Image = Image;
+    }
+//
+    get theDescription()
+    {
+        return this.Description;
+    }
+    set theDescription(Description) 
+    {
+        this.Description = Description;
+    }
+//
+    get theShape()
+    {
+        return this.Shape;
+    }
+    set theShape(Shape) 
+    {
+        this.Shape = Shape;
+    }
+}
+
+
+// get an object from array
+function accessInformation()
+{
+    
+    var randomNumber = Math.floor(Math.random() * myViewFinderArray.length);
+    console.log(randomNumber)
+    document.getElementById("title").innerHTML = myViewFinderArray[randomNumber].toString();
+
+}*/
+}
+function moveSquare()
+{
+    $("#square").fadeIn("slow").animate({right:250}).animate({bottom:400}).animate({left:250}).animate({top:400}).fadeOut("slow");
+    $("#square2").fadeIn("slow").animate({left:250}).animate({top:400}).animate({right:250}).animate({bottom:400}).fadeOut("slow");
+    $("#square3").fadeIn("slow").animate({top:400}).animate({bottom:400}).fadeOut("slow");
+}
+
+$(document).ready(function () {
+    $('h1').hide();
+    $('b').hide();
+    $('p').hide();
+    $("#image1").hide();
+    $("#image2").hide();
+    $("#image3").hide();
+    $("#square").hide();
+    $("#square2").hide();
+    $("#square3").hide();
+});
+//why is it not hiding the images at first, but does for the text and squares?
+
+//I dont understand how to use arrays to make certain images and text pop up, or how to make my text get bigger and smaller.
+
+$(document).ready(function () {
+    $("button").click(function () {
+       $('h1').fadeIn("slow").css({'font-size' : '5px'}).css({'font-size' : '20px'});
+       $("#image1").fadeIn("slow").fadeOut("slow").fadeToggle();
+       moveSquare()
+});
+})
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $('b').fadeIn("slow").css({'font-size' : '5px'}).css({'font-size' : '20px'});
+        $("#image2").fadeIn("slow").fadeOut("slow").fadeToggle();   
+    });
+})
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $('p').fadeIn("slow").css({'font-size' : '5px'}).css({'font-size' : '20px'});
+        $("#image3").fadeIn("slow").fadeOut("slow").fadeToggle();   
+    });
+})
+
+
+
+
+
+
+
+
+
+//
+/*   
+}
+var imageTags = ["image1, image2, image3"]
+var myViewFinderArray = new Array();
+function initializeArray()
+{
+    
+    var myViewFinder1 = new ViewFinder('images/image1.jpg', "Me when I see how hard my coding homework is", '#square')
+   
+    var myViewFinder2 = new ViewFinder('images/image2.jpg', "Me currently trying to figure out coding homework", '#square2');
+    
+   
+    var myViewFinder3 = new ViewFinder('images/image3.jpeg', "Me after finishign coding homework and realizing I did not retain anything", '#square3');
+
+   
+    myViewFinderArray.push(myViewFinder1);
+    myViewFinderArray.push(myViewFinder2);
+    myViewFinderArray.push(myViewFinder3);
+   
+}*/
+
+//????TRYING FOR IMAGES HERE????????????
+
+
+/*var actualImages = [];
+var actualImages = new Array();
+
+function createRandomImageArray() {
+    // Clear previous images
+    actualImages = [];
+    // create an array of actual images
+    var actualImagePath = ["images/image1.jpg", "images/image2.jpg", "images/image3.jpeg",];
+    // create a count array to track how many times an image has been added
+    var count = [0, 0, 0, 0, 0,]; // count for each image path
+}
+
+function imagesDisappear() {
+    // Assuming "blankImagePath" is defined somewhere or add it here
+    var blankImagePath = "images/blank.jpg";
+    imageTags.forEach(function(tag) {
+        document.getElementById(tag).src = blankImagePath;
+    });
+}*/
+
+/*jquery that animates stuff-
+$(document).ready(function () {
+    $("button").click(function () {
+        $(".myGreatClass").animate({left: '250px'});
+ });
+            
+jquery that fade in and out slow-
+$(document).ready(function () {
+    $("button").click(function () {
+       $(".myGreatClass").fadeOut("slow").fadeIn("slow");
+});
+
+jquery that shows text?
+$(document).ready(function () {
+    $("button").click(function () {
+        window.alert($("#infoid").text());
+});
+
+jquery examples?
+$(document).ready(function () {
+            $("button").click(function () {
+                // $("p").toggle();
+               // $(this).hide();
+                //  $(".info").toggle();
+               // $("#infoid").fadeToggle();
+            });
+*/
